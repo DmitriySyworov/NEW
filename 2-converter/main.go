546a0b2d1c736771f6778989e894318262e4f076
Dmitriy_Syworov <dmitriysyworov1986.com@gmail.com>
2025-09-25 19:48:03 +0000

    fresh-fix

--- 2-converter/main.go
+++ 2-converter/main.go
@@ -3,19 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	number, eur, usd := output()
-	resultEUR, resultUSD := countCurrencies(number, eur, usd)
-	fmt.Printf("Указанное количество рублей = %.2f EUR \n", resultEUR)
-	fmt.Printf("указанное количество рублей = %.2f USD", resultUSD)
+	number, orig, targ := output()
+	rez := countCurrencies(number, orig, targ)
+
 }
-func output() (float64, float64, float64) {
-	var numberCurrency, EUR, USD float64
-	fmt.Println("Укажите количество рублей и через пробел курс валюты евро и доллара по отношению к рублю")
-	fmt.Scan(&numberCurrency, &EUR, &USD)
-	return numberCurrency, EUR, USD
+func output() (float64, string, string) {
+	var numberCurrency float64
+	var original, target string
+	fmt.Scan(&numberCurrency, &original, &target)
+	return numberCurrency, original, target
 }
-func countCurrencies(numbers, EUR, USD float64) (float64, float64) {
-	rez := numbers / EUR
-	rez2 := numbers / USD
-	return rez, rez2
+func countCurrencies(numbers float64, Original, Target string) float64 {
+
 }
